358abd7998d53146bc1e8b541c8a8e748bcd3890
Lajith55 <134773076+Lajith55@users.noreply.github.com>
2023-05-29 18:54:47 +0530

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,10 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: C:/Users/user/Desktop/python/1st level.py ==============
+Hello, World!
+>>> 
+============== RESTART: C:/Users/user/Desktop/python/1st level.py ==============
+Hello, World!
+What is your name?lajith
+Hello lajith
